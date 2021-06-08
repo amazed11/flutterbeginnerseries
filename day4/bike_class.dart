ea94db95234d6b7742b947b46a09b8c07a76a35e
amazed11 <amazedking2@gmail.com>
2021-06-08 12:57:21 +0545

    second commit

--- day4/bike_class.dart
+++ day4/bike_class.dart
@@ -1,12 +1,16 @@
 /*
 String Concatenation: It is combining of any two string
-eg "ram" +" "+ "shyam"="ram shyam"
+eg "ram" + " " + "shyam"="ram shyam"
 eg "1" + "2"="12"
 
 Method Return type inside class
 :It expects to return the same  datatype  
 as in return data type.
 you must use void for no any return datatype.
+
+Null safety
+late keyword in properties makes properties to
+accept a value in future but not making null.
 */
 
 void main() {
