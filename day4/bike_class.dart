26a9c391b3e9da05f9d4476241cc233626897053
amazed11 <amazedking2@gmail.com>
2021-06-08 12:55:32 +0545

    first commit

---
+++ day4/bike_class.dart
@@ -0,0 +1,34 @@
+/*
+String Concatenation: It is combining of any two string
+eg "ram" +" "+ "shyam"="ram shyam"
+eg "1" + "2"="12"
+
+Method Return type inside class
+:It expects to return the same  datatype  
+as in return data type.
+you must use void for no any return datatype.
+*/
+
+void main() {
+  Bike bikeObj = Bike();
+  // bikeObj.id = 9;
+  print(bikeObj.ride());
+  //string concatenation
+  // var complete = total + " " + bikeObj.ride(); //ram + is a rider
+}
+
+class Bike {
+  //properties
+  late int id;
+  late String bikeName;
+  late double price;
+
+  //methods or behaviours
+  DateTime ride() {
+    return DateTime.now();
+  }
+
+  void stand() {
+    print("I am stading");
+  }
+}
